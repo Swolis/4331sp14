8fd20ef9638f54421557e32c23d4c6c6a126cc43
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-01 16:28:07 -0400

    Update register.php

--- Frontend/register/register.php
+++ Frontend/register/register.php
@@ -23,7 +23,7 @@
         <!--end of nav-->
 
     <div class="container">
-         <?php
+                 <?php
             $username = "";
             $password = "";
             $firstName ="";
@@ -34,20 +34,81 @@
             $chessRating = 0;
             $favoriteOpening = "";
             $title = "";
-?php>
+
+            // Check if the form is submitted
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+                // Get the username, password, and additional user information from the form
+                $username = $_POST["username"];
+                $password = $_POST["password"];
+                $firstName = $_POST["firstName"];
+                $lastName = $_POST["lastName"];
+                $email = $_POST["email"];
+                $phone = $_POST["phone"];
+                $country = $_POST["country"];
+                $chessRating = $_POST["chessRating"];
+                $favoriteOpening = $_POST["favoriteOpening"];
+                $title = $_POST["title"];
+
+                // Connect to the database
+                $conn = new mysqli("localhost", "newuser", "StrongerPassword123!", "chesscont");
+
+                // Check the connection
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                // Hash the password
+                $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+                // Prepare a SQL statement to insert the new user
+                $stmt = $conn->prepare("INSERT INTO users (username, password, first_name, last_name, email, phone, country, chess_rating, favorite_opening, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                $stmt->bind_param("ssssssssss", $username, $hashed_password, $firstName, $lastName, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
+
+                $select = mysqli_query($conn, "SELECT * FROM users WHERE username = '".$_POST['username']."'");
+
+                // Execute the statement
+                if (!mysqli_num_rows($select)&&$stmt->execute()) {
+                    // Registration successful, redirect to the login page
+                    header("Location: ../login/login.php");
+                    exit();
+                }else {
+                    // Registration failed, redirect back to the registration page with an error
+
+
+                echo('Sorry, the Username '.$_POST['username'].' has already been used to register.');
+                $password = $_POST["password"];
+                $firstName = $_POST["firstName"];
+                $lastName = $_POST["lastName"];
+                $email = $_POST["email"];
+                $phone = $_POST["phone"];
+                $country = $_POST["country"];
+                $chessRating = $_POST["chessRating"];
+                $favoriteOpening = $_POST["favoriteOpening"];
+                $title = $_POST["title"];
+
+
+                }
+
+                // Close the connection
+                $stmt->close();
+                $conn->close();
+            }
+        ?>
+
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card mt-5">
                     <div class="card-header">Register</div>
                     <div class="card-body">
-                        <form method="POST" action="process-register.php">
+                        <form method="POST" action="register.php">
                             <div class="mb-3">
                                 <label for="username" class="form-label">Username</label>
                                 <input type="text" class="form-control" id="username" name="username" required>
                             </div>
                             <div class="mb-3">
                                 <label for="password" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="password" name="password"value="<?php echo $password ?>" required>
+                                <input type="password" class="form-control" id="password" name="password" value="<?php echo $password ?>"required>
                             </div>
                             <div class="mb-3">
                                 <label for="firstName" class="form-label">First Name</label>
@@ -55,7 +116,7 @@
                             </div>
                             <div class="mb-3">
                                 <label for="lastName" class="form-label">Last Name</label>
-                                <input type="text" class="form-control" id="lastName" name="lastName"value="<?php echo $firstName ?>">
+                                <input type="text" class="form-control" id="lastName" name="lastName"value="<?php echo $lastName ?>">
                             </div>
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email</label>
@@ -63,11 +124,11 @@
                             </div>
                             <div class="mb-3">
                                 <label for="phone" class="form-label">Phone</label>
-                                <input type="text" class="form-control" id="phone" name="phone"value="<?php echo $phone ?>">
+                                <input type="text" class="form-control" id="phone" name="phone" value="<?php echo $phone ?>">
                             </div>
                             <div class="mb-3">
                                 <label for="country" class="form-label">Country</label>
-                                <input type="text" class="form-control" id="country" name="country"value="<?php echo $country?>>
+                                <input type="text" class="form-control" id="country" name="country" value="<?php echo $country?>">
                             </div>
                             <div class="mb-3">
                                 <label for="chessRating" class="form-label">Chess Rating</label>
@@ -75,11 +136,11 @@
                             </div>
                             <div class="mb-3">
                                 <label for="favoriteOpening" class="form-label">Favorite Opening</label>
-                                <input type="text" class="form-control" id="favoriteOpening" name="favoriteOpening"<?php echo $favoriteOpening ?>">
+                                <input type="text" class="form-control" id="favoriteOpening" name="favoriteOpening"value="<?php echo $favoriteOpening ?>">
                             </div>
                             <div class="mb-3">
                                 <label for="title" class="form-label">Title</label>
-                                <input type="text" class="form-control" id="title" name="title""value="<?php echo $title ?>">
+                                <input type="text" class="form-control" id="title" name="title"value="<?php echo $title ?>">
                             </div>
                             <!-- Add fields for other user information (email, country, etc.) -->
                             <button type="submit" class="btn btn-primary">Register</button>
