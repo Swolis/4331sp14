eb566cadcf591f5592be0f6da0a5308ccd714efc
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-02 15:31:47 -0400

    Update edit.php

--- Frontend/profile/edit.php
+++ Frontend/profile/edit.php
@@ -11,7 +11,7 @@
         }
         echo "hi";
         // Prepare a SQL statement to insert the new user
-        if(isset($_GET['id']){
+        if(isset($_SESSION['user_id']){
         $id = $_GET['id'];
             echo $id;
         $name = $_POST['newName'];
