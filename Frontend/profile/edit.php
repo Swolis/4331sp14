64c8198e6eb43ea103dd4604cd6115a4ed4907fe
Daniel Solis <danielasolis.cs@gmail.com>
2023-10-02 14:04:16 -0400

    d

--- Frontend/profile/edit.php
+++ Frontend/profile/edit.php
@@ -1,39 +1,42 @@
 <?php
 // Connect to the database
-    $conn = new mysqli("localhost", "newuser", "StrongerPassword123!", "chesscont");
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // Check the connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+        $conn = new mysqli("localhost", "newuser", "StrongerPassword123!", "chesscont");
 
-    // Prepare a SQL statement to insert the new user
-    //if(isset($_GET['id'])) {
-    $id = $_POST['id'];
-    $name = $_POST['newName'];
-    $email = $_POST['newEmail'];
-    $phone = $_POST['newPhone'];
-    $country = $_POST['newCountry'];
-    $chessRating = $_POST['newRating'];
-    $favoriteOpening = $_POST['newOpening'];
-    $title = $_POST['newTitle'];
-    $address = $_POST['newAddress'];
-    $notes = $_POST['newNotes'];
+        // Check the connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
 
-    $stmt = $conn->prepare("UPDATE contacts SET name = ".$name.", email = ".$email.", phone=".$phone.", country=".$country.", chess_rating=".$chessRating.", favorite_opening=".$favoriteOpening.", title=".$title.", address=".$address.", notes=".$notes." WHERE id =".$id.);
-        //$stmt->bind_param("i", $id);  // Assuming 'id' is an integer
-    $stmt->execute();
+        // Prepare a SQL statement to insert the new user
+        //if(isset($_GET['id'])) {
+        $id = $_POST['id'];
+        $name = $_POST['newName'];
+        $email = $_POST['newEmail'];
+        $phone = $_POST['newPhone'];
+        $country = $_POST['newCountry'];
+        $chessRating = $_POST['newRating'];
+        $favoriteOpening = $_POST['newOpening'];
+        $title = $_POST['newTitle'];
+        $address = $_POST['newAddress'];
+        $notes = $_POST['newNotes'];
+
+        $stmt = $conn->prepare("UPDATE contacts SET name = ".$name.", email = ".$email.", phone=".$phone.", country=".$country.", chess_rating=".$chessRating.", favorite_opening=".$favoriteOpening.", title=".$title.", address=".$address.", notes=".$notes." WHERE id =".$id.);
+            //$stmt->bind_param("i", $id);  // Assuming 'id' is an integer
+        $stmt->execute();
+            
+            
+
+        //}
         
-        
 
-    //}
+        // Execute the statement
     
 
-    // Execute the statement
-   
-
-    // Close the connection
-    $stmt->close();
-    $conn->close();
-    header("Location: https://chessconnect.xyz/profile/");
+        // Close the connection
+        $stmt->close();
+        $conn->close();
+        header("Location: https://chessconnect.xyz/profile/");
+    }
 ?>
