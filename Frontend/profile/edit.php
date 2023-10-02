2237f775f6c468b6d54d0c1077ad9802cdbeb2da
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-02 14:58:03 -0400

    Update edit.php

--- Frontend/profile/edit.php
+++ Frontend/profile/edit.php
@@ -27,9 +27,10 @@
         $address = $_POST['newAddress'];
          
         $notes = $_POST['newNotes'];
-/*
+
         $stmt = $conn->prepare("UPDATE contacts SET name = ".$name.", email = ".$email.", phone=".$phone.", country=".$country.", chess_rating=".$chessRating.", favorite_opening=".$favoriteOpening.", title=".$title.", address=".$address.", notes=".$notes." WHERE id =".$id.);
-         $stmt->bind_param("issssi", $id);  // Assuming 'id' is an integer
+        /*
+           $stmt->bind_param("issssi", $id);  // Assuming 'id' is an integer
         $stmt->execute();
             
             
