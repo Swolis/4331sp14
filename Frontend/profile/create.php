24de7bd46b2d9f278b63988f069cfb0f604ae034
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-01 20:35:00 -0400

    Update create.php

--- Frontend/profile/create.php
+++ Frontend/profile/create.php
@@ -7,21 +7,21 @@
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
-        if (!isset($_SESSION["user_id"])) {
+        if (!isset($_SESSION['user_id'])) {
             die("User ID not found in session. Please log in again.");
         }
         
         
         // Assuming you pass user_id from some other source like session
-        if(isset($_GET['id']){
-        $userId = $_SESSION["user_id"];
-        $name = $_POST["name"];
-        $email = $_POST["email"] ?? null;  // Uses null coalescing for optional fields
-        $phone = $_POST["phone"] ?? null;
-        $country = $_POST["country"] ?? null;
-        $chessRating = $_POST["chessRating"] ?? null;
-        $favoriteOpening = $_POST["favoriteOpening"] ?? null;
-        $title = $_POST["title"] ?? null;
+        if(isset($_GET['user_id']){
+        $userId = $_SESSION['user_id'];
+        $name = $_POST['name'];
+        $email = $_POST['email'] ?? null;  // Uses null coalescing for optional fields
+        $phone = $_POST['phone'] ?? null;
+        $country = $_POST['country'] ?? null;
+        $chessRating = $_POST['chessRating'] ?? null;
+        $favoriteOpening = $_POST['favoriteOpening'] ?? null;
+        $title = $_POST['title'] ?? null;
     
         $stmt = $conn->prepare("INSERT INTO contacts (user_id, name, email, phone, country, chess_rating, favorite_opening, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
         $stmt->bind_param("issssiss", $userId, $name, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
