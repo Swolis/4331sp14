a5ca54adccce44e8e4f27a69d7fd644c538862f2
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-02 19:23:28 -0400

    Update edit.php

--- Frontend/profile/edit.php
+++ Frontend/profile/edit.php
@@ -1,12 +1,12 @@
 <?php
 // Connect to the database
-    echo "pain";
+ 
 session_start();
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $conn = new mysqli("localhost", "newuser", "StrongerPassword123!", "chesscont");
-echo "hi";
+
         // Check the connection
         if ($conn->connect_error) {
             die("Connection failure");
@@ -14,18 +14,16 @@ echo "hi";
         
         // Prepare a SQL statement to insert the new user
         if(isset($_SESSION['user_id'])) {
-        $id = $_POST['id'];
-            echo $id;
-        $name = $_POST['newName'];
-            echo $name;
-        $email = $_POST['newEmail'];
-        $phone = $_POST['newPhone'];
-        $country = $_POST['newCountry'];
-        $chessRating = $_POST['newRating'];
-        $favoriteOpening = $_POST['newOpening'];
-        $title = $_POST['newTitle'];
-        $address = $_POST['newAddress'];
-          $notes=$_POST['newNotes']  ;
+            $id = $_POST['id'];
+            $name = $_POST['newName'];
+            $email = $_POST['newEmail'];
+            $phone = $_POST['newPhone'];
+            $country = $_POST['newCountry'];
+            $chessRating = $_POST['newRating'];
+            $favoriteOpening = $_POST['newOpening'];
+            $title = $_POST['newTitle'];
+            $address = $_POST['newAddress'];
+            $notes=$_POST['newNotes']  ;
 
         $stmt = $conn->prepare("UPDATE contacts SET name = '$name', email = '$email', phone='$phone', country='$country', chess_rating='$chessRating', favorite_opening='$favoriteOpening', title='$title', address='$address', notes='$notes' WHERE id ='$id'");
        // $stmt->bind_param("ssssisss", $name,$email,$phone,$country,$chessRating,$favoriteOpening,$title,$address,$notes);  // Assuming 'id' is an integer
