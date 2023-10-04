000d0d09eda10e5f16b867ec3962bd140e853cb8
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 12:23:43 -0400

    Update process-register.php

--- Frontend/register/process-register.php
+++ Frontend/register/process-register.php
@@ -28,7 +28,8 @@ if ($conn->connect_error) {
         exit();
     }
     $stmt = $conn->prepare("INSERT INTO users (username, password, first_name, last_name, email, phone, country, chess_rating, favorite_opening, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-                $stmt->bind_param("sssssssiss", $username, $hashed_password, $firstName, $lastName, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
+    echo $username;
+    $stmt->bind_param("sssssssiss", $username, $hashed_password, $firstName, $lastName, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
 
  $stmt->close();
    $conn->close();
