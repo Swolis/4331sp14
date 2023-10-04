dce66988a54cf2d6833b3fd3206db9960fee8879
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 02:08:58 -0400

    Update process-register.php

--- Frontend/register/process-register.php
+++ Frontend/register/process-register.php
@@ -2,7 +2,14 @@
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     $username=$_POST['username'];
     $password=$_POST['password'];
-    echo "hi";
+     $firstName=$_POST['firstName'];
+    $lastName=$_POST['lastName'];
+     $email=$_POST['email'];
+    $phone=$_POST['phone'];
+     $country=$_POST['country'];
+    $chessRating=$_POST['chessRating'];
+     $favoriteOpening=$_POST['favoriteOpening'];
+    $title=$_POST['title'];
     
 }
     
