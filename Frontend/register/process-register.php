afb6cbdc79c1f13809471c6e83177ace5944ca1b
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 12:30:46 -0400

    Update process-register.php

--- Frontend/register/process-register.php
+++ Frontend/register/process-register.php
@@ -1,16 +1,16 @@
 <?php
 
-//if($_SERVER["REQUEST_METHOD"]=="POST"){
+if($_SERVER["REQUEST_METHOD"]=="POST"){
     $username=$_GET['username'];
-    $password=$_POST['password'];
-     $firstName=$_POST['firstName'];
-    $lastName=$_POST['lastName'];
-     $email=$_POST['email'];
-    $phone=$_POST['phone'];
-     $country=$_POST['country'];
-    $chessRating=$_POST['chessRating'];
-     $favoriteOpening=$_POST['favoriteOpening'];
-    $title=$_POST['title'];
+    $password=$_GET['password'];
+     $firstName=$_GET['firstName'];
+    $lastName=$_GET['lastName'];
+     $email=$_GET['email'];
+    $phone=$_GET['phone'];
+     $country=$_GET['country'];
+    $chessRating=$_GET['chessRating'];
+     $favoriteOpening=$_GET['favoriteOpening'];
+    $title=$_GET['title'];
     $conn = new mysqli("localhost", "newuser", "StrongerPassword123!", "chesscont");
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
@@ -27,12 +27,15 @@ if ($conn->connect_error) {
         header("Location:../register/register.php");
         exit();
     }
+    
     $stmt = $conn->prepare("INSERT INTO users (username, password, first_name, last_name, email, phone, country, chess_rating, favorite_opening, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
     echo "hi";
     $stmt->bind_param("sssssssiss", $username, $hashed_password, $firstName, $lastName, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
+    if($stmt->execute()){
+        header("Location: ..login/login.php");
+    }
 echo $username;
  $stmt->close();
-   $conn->close();
-//}
+   $conn->close();}
     
 ?>
