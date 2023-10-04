16f8ac0e8d5dbf0fac59f52aeea7d880f30f09b4
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 00:26:47 -0400

    Update register.php

--- Frontend/register/register.php
+++ Frontend/register/register.php
@@ -142,7 +142,7 @@
                             <script>
                                    formSubmission.addEventListener ("click",function(){
                                         var sendThis=new XMLHttpRequest();
-                                        sendThis.open("POST","process-register.php",true);
+                                        sendThis.open("POST","https://chessconnect.xyz/register//process-register.php",true);
                                        var user="username="+document.getElementById("username").innerHTML+"&"+"password="+document.getElementById("password").innerHTML+"&"+"firstName="+document.getElementById("firstName").innerHTML+"&"+"lastName="+document.getElementById("lastName").innerHTML+"&"+"phone="+document.getElementById("phone").innerHTML+"&"+"email="+document.getElementById("email").innerHTML+"&"+"country="+document.getElementById("country").innerHTML+"&"+"chessRating="+document.getElementById("chessRating").innerHTML+"&"+"favoriteOpening="+document.getElementById("favoriteOpening").innerHTML+"&"+"title="+document.getElementById("title").innerHTML;
                                        sendThis.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
                                        sendThis.send(user);
