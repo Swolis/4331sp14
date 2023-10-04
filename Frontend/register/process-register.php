412e83eff06d20122c472887be67cd61b9db7a13
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 16:28:12 -0400

    Update process-register.php

--- Frontend/register/process-register.php
+++ Frontend/register/process-register.php
@@ -1,5 +1,12 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 
+error_reporting(E_ALL); // reports all errors
+ini_set("display_errors", "1"); // shows all errors
+ini_set("log_errors", 1);
+ini_set("error_log", "/tmp/php-error.log");
 if($_SERVER["REQUEST_METHOD"]=="GET"){
     $username=$_POST['username'];
     
