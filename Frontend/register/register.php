f28f10f7d913d4ca6c1c1a83c4ef8af6a9df9c21
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 17:15:36 -0400

    Update register.php

--- Frontend/register/register.php
+++ Frontend/register/register.php
@@ -143,7 +143,7 @@
                                    formSubmission.addEventListener ("click",function(){
                                         //var sendThis=new XMLHttpRequest();
                                          var url = "process-register.php";
-                                       var user="username="+document.getElementById("username").value+"&"+"password="+document.getElementById("password").value+"&"+"firstName="+document.getElementById("firstName").innerHTML+"&"+"lastName="+document.getElementById("lastName").value+"&"+"email="+document.getElementById("email").value+"&"+"phone="+document.getElementById("phone").value+"&"+"country="+document.getElementById("country").value+"&"+"chessRating="+document.getElementById("chessRating").value+"&"+"favoriteOpening="+document.getElementById("favoriteOpening").value+"&"+"title="+document.getElementById("title").value;
+                                       var user="username="+document.getElementById("username").value+"&"+"password="+document.getElementById("password").value+"&"+"firstName="+document.getElementById("firstName").value+"&"+"lastName="+document.getElementById("lastName").value+"&"+"email="+document.getElementById("email").value+"&"+"phone="+document.getElementById("phone").value+"&"+"country="+document.getElementById("country").value+"&"+"chessRating="+document.getElementById("chessRating").value+"&"+"favoriteOpening="+document.getElementById("favoriteOpening").value+"&"+"title="+document.getElementById("title").value;
                                        
                                        sendThis.open("GET",url,true);
                                        sendThis.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
