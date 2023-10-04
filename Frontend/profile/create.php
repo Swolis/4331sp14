3e61c79c822ee049159ccfde63a3e799baf28d63
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 13:19:33 -0400

    Update create.php

--- Frontend/profile/create.php
+++ Frontend/profile/create.php
@@ -20,15 +20,15 @@ echo $_GET['chessRating'];
         // Assuming you pass user_id from some other source like session
         //$userId = $_SESSION['user_id'];
         $name = $_GET['name'];
-        $email = $_GET['email'] ?? null;  // Uses null coalescing for optional fields
+       // $email = $_GET['email'] ?? null;  // Uses null coalescing for optional fields
         $phone = $_GET['phone'] ?? null;
         $country = $_GET['country'] ?? null;
         $chessRating = $_GET['chessRating'] ?? null;
         $favoriteOpening = $_GET['favoriteOpening'] ?? null;
         $title = $_GET['title'] ?? null;
    
-        //$stmt = $conn->prepare("INSERT INTO contacts (user_id, name, email, phone, country, chess_rating, favorite_opening, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-        //$stmt->bind_param("issssiss", $userId, $name, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
+        //$stmt = $conn->prepare("INSERT INTO contacts (user_id, name, phone, country, chess_rating, favorite_opening, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+        //$stmt->bind_param("isssiss", $userId, $name,  $phone, $country, $chessRating, $favoriteOpening, $title);
         //$stmt->execute();
         //header("Location: ../profile/");
     }
