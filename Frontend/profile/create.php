c5cc422bc3eadacc36225f18fef31568b9fb0920
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 13:12:48 -0400

    Update create.php

--- Frontend/profile/create.php
+++ Frontend/profile/create.php
@@ -8,13 +8,13 @@
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
-        if (!isset($_SESSION['user_id'])) {
+       /* if (!isset($_SESSION['user_id'])) {
             die("User ID not found in session. Please log in again.");
         }
-        
+        */
         echo "hi";
         // Assuming you pass user_id from some other source like session
-        $userId = $_SESSION['user_id'];
+        //$userId = $_SESSION['user_id'];
         $name = $_GET['name'];
         $email = $_GET['email'] ?? null;  // Uses null coalescing for optional fields
         $phone = $_GET['phone'] ?? null;
@@ -22,10 +22,10 @@
         $chessRating = $_GET['chessRating'] ?? null;
         $favoriteOpening = $_GET['favoriteOpening'] ?? null;
         $title = $_GET['title'] ?? null;
-    
-        $stmt = $conn->prepare("INSERT INTO contacts (user_id, name, email, phone, country, chess_rating, favorite_opening, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("issssiss", $userId, $name, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
-        $stmt->execute();
+    echo $name;
+        //$stmt = $conn->prepare("INSERT INTO contacts (user_id, name, email, phone, country, chess_rating, favorite_opening, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+        //$stmt->bind_param("issssiss", $userId, $name, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
+        //$stmt->execute();
         //header("Location: ../profile/");
     }
 
