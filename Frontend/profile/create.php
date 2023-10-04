b9debbc28d6e7620c42a82f2b56eb1191b62fe4f
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 15:19:31 -0400

    Update create.php

--- Frontend/profile/create.php
+++ Frontend/profile/create.php
@@ -13,18 +13,18 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
    
    
 $userid=$_SESSION['user_id'];
-  $name=$_GET['name']??null;
+  $name=$_GET['name'];
   $email=$_GET['email']??null;
   $phone=$_GET['phone']??null;
   $country=$_GET['country']??null;
   $chessRating=$_GET['chessRating']??null;
   $favoriteOpening=$_GET['favoriteOpening']??null;
   $title=$_GET['title']??null;
-  
+  /*
 $stmt = $conn->prepare("INSERT INTO contacts (user_id,name, email, phone, country, chess_rating, favorite_opening, title) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?)");
  $stmt->bind_param("issssiss", $userId, $name, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
  $stmt->execute();
  header("https://chessconnect.xyz/");
-   
+   */
 }
 ?>
