650c09fbac23243a197601abc64c67cc7f9152cb
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 12:44:37 -0400

    Update create.php

--- Frontend/profile/create.php
+++ Frontend/profile/create.php
@@ -14,13 +14,13 @@
         
         // Assuming you pass user_id from some other source like session
         $userId = $_SESSION['user_id'];
-        $name = $_POST['name'];
-        $email = $_POST['email'] ?? null;  // Uses null coalescing for optional fields
-        $phone = $_POST['phone'] ?? null;
-        $country = $_POST['country'] ?? null;
-        $chessRating = $_POST['chessRating'] ?? null;
-        $favoriteOpening = $_POST['favoriteOpening'] ?? null;
-        $title = $_POST['title'] ?? null;
+        $name = $_GET['name'];
+        $email = $_GET['email'] ?? null;  // Uses null coalescing for optional fields
+        $phone = $_GET['phone'] ?? null;
+        $country = $_GET['country'] ?? null;
+        $chessRating = $_GET['chessRating'] ?? null;
+        $favoriteOpening = $_GET['favoriteOpening'] ?? null;
+        $title = $_GET['title'] ?? null;
     
         $stmt = $conn->prepare("INSERT INTO contacts (user_id, name, email, phone, country, chess_rating, favorite_opening, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
         $stmt->bind_param("issssiss", $userId, $name, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
