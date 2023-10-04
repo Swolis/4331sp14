35cbaf876e50202b207cff6ae63126d0b07fc529
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 01:45:23 -0400

    Update process-register.php

--- Frontend/register/process-register.php
+++ Frontend/register/process-register.php
@@ -7,6 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the username, password, and additional user information from the form
     $username = $_POST["username"];
     echo $_POST["username"];
+    /*
     $password = $_POST["password"];
     $firstName = $_POST["firstName"];
     $lastName = $_POST["lastName"];
@@ -43,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       exit();
   }
         
-    
+    */
 
     // Close the connection
     $stmt->close();
