36374ecfb87e88603b96117f5d4f85dca1e468ba
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 00:50:31 -0400

    Update register.php

--- Frontend/register/register.php
+++ Frontend/register/register.php
@@ -96,7 +96,7 @@
                 <div class="card mt-5">
                     <div class="card-header">Register</div>
                     <div class="card-body">
-                        <form method="POST" action="process-register.php">
+                        <form>
                             <div class="mb-3">
                                 <label for="username" class="form-label">Username</label>
                                 <input type="text" class="form-control" id="username" name="username" required>
