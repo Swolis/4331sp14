cbba64711c17a7c5adbdc2221a5423450c601db9
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 00:38:54 -0400

    Update process-register.php

--- Frontend/register/process-register.php
+++ Frontend/register/process-register.php
@@ -1,10 +1,6 @@
 <?php
 // Start session
-session_start();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-var_dump($_POST);
+
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
