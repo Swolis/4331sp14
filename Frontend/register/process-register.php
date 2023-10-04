93603fe8bc3f2a5ede0ff4efe2f780e26686262c
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 19:06:42 -0400

    Update process-register.php

--- Frontend/register/process-register.php
+++ Frontend/register/process-register.php
@@ -47,5 +47,20 @@ if ($conn->connect_error) {
  $stmt->close();
    $conn->close();
 }
-    
+    	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
 ?>
