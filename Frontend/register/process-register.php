750d0fe4b6d11425d6528142170398a8b3434505
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 01:46:24 -0400

    Update process-register.php

--- Frontend/register/process-register.php
+++ Frontend/register/process-register.php
@@ -6,7 +6,7 @@ echo "hi";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the username, password, and additional user information from the form
     $username = $_POST["username"];
-    echo $_POST["username"];
+    //echo $_POST["username"];
     /*
     $password = $_POST["password"];
     $firstName = $_POST["firstName"];
