a076368ace44201023adc043965b66cfb608aed7
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 02:16:52 -0400

    Update process-register.php

--- Frontend/register/process-register.php
+++ Frontend/register/process-register.php
@@ -10,7 +10,15 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     $chessRating=$_POST['chessRating'];
      $favoriteOpening=$_POST['favoriteOpening'];
     $title=$_POST['title'];
-    
+    $conn = new mysqli("localhost", "newuser", "StrongerPassword123!", "chesscont");
+    $hashedPassword=password_hash($password,PASSWORD_DEFAULT);
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+     $stmt = $conn->prepare("INSERT INTO users (username,password, first_name, last_name,email, phone, country, chess_rating, favorite_opening, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?,?)");
+    $stmt->bind_param("sssssssiss",$username,$hashedPassword,$firstName,$lastName,$email,$phone,$country,$chessRating,$favoriteOpening,$title);
+    echo "hi";
 }
     
 ?>
