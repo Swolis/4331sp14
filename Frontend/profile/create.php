dbf09d0b2e4bfc1b5c275cdbd2de2511304bf170
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 13:59:46 -0400

    Update create.php

--- Frontend/profile/create.php
+++ Frontend/profile/create.php
@@ -1,4 +1,24 @@
 <?php
 session_start();
 echo $_GET['username'];
+if($_SERVER['REQUEST_METHOD']=="POST"){
+   $conn = new mysqli("localhost", "newuser", "StrongerPassword123!", "chesscont");
+   if ($conn->connect_error) {
+            die("Connection failure");
+    }
+  $userid=$_SESSION['user_id'];
+  if(!isset[$userid]){
+    die("User not found");
+  }
+  $name=$_GET['name'];
+  $email=$_GET['email'];
+  $phone=$_GET['phone'];
+  $country=$_GET['country'];
+  $chessRating=$_GET['chessRating'];
+  $favoriteOpening=$_GET['favoriteOpening'];
+  $title=$_GET['title'];
+  
+  $stmt = $conn->prepare("INSERT INTO contacts (user_id,name, email, phone, country, chess_rating, favorite_opening, title) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?)");
+  $stmt->bind_param
+}
 ?>
