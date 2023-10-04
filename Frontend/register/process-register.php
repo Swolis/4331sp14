4a08955b88bd9af70d3b86e5de5662a99f002b1b
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 16:03:23 -0400

    Update process-register.php

--- Frontend/register/process-register.php
+++ Frontend/register/process-register.php
@@ -1,16 +1,16 @@
 <?php
 
 if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $username=$_GET['username'];
-    $password=$_GET['password'];
-     $firstName=$_GET['firstName'];
-    $lastName=$_GET['lastName'];
-     $email=$_GET['email'];
-    $phone=$_GET['phone'];
-     $country=$_GET['country'];
-    $chessRating=$_GET['chessRating'];
-     $favoriteOpening=$_GET['favoriteOpening'];
-    $title=$_GET['title'];
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+     $firstName=$_POST['firstName'];
+    $lastName=$_POST['lastName'];
+     $email=$_POST['email'];
+    $phone=$_POST['phone'];
+     $country=$_POST['country'];
+    $chessRating=$_POST['chessRating'];
+     $favoriteOpening=$_POST['favoriteOpening'];
+    $title=$_POST['title'];
     $conn = new mysqli("localhost", "newuser", "StrongerPassword123!", "chesscont");
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
