f9e33f52e606ce7d2c2c608b500cd26be74f3404
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 02:02:09 -0400

    Update process-register.php

--- Frontend/register/process-register.php
+++ Frontend/register/process-register.php
@@ -1,43 +1,4 @@
 <?php
-// Start session
-//echo "hi";
 
-// Check if the form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    //Get the username, password, and additional user information from the form
-    $username = $_POST["username"];
-    
-    
-    $password = $_POST["password"];
-    $firstName = $_POST["firstName"];
-    $lastName = $_POST["lastName"];
-    $email = $_POST["email"];
-    $phone = $_POST["phone"];
-    $country = $_POST["country"];
-    $chessRating = $_POST["chessRating"];
-    $favoriteOpening = $_POST["favoriteOpening"];
-    $title = $_POST["title"];
-
-    // Connect to the database
-    $conn = new mysqli("localhost", "newuser", "StrongerPassword123!", "chesscont");
-
-    // Check the connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    header("Location: ../login/")
-
-    // Hash the password
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-    // Prepare a SQL statement to insert the new user
-    //$stmt = $conn->prepare("INSERT INTO users (username, password, first_name, last_name, email, phone, country, chess_rating, favorite_opening, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-    
-    //$stmt->bind_param("sssssssiss", $username, $hashed_password, $firstName, $lastName, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
-    echo "hi';
-    
-   
-    
-}
     
 ?>
