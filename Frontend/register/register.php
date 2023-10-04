8cafeb37ce72aadd71522db7e55c5a32b68bf11c
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 19:47:32 -0400

    Update register.php

--- Frontend/register/register.php
+++ Frontend/register/register.php
@@ -141,16 +141,25 @@
                             <button type="submit" id="formSubmission" class="btn btn-primary">Register</button>
                             <script>
                                    formSubmission.addEventListener ("click",function(){
-                                        var sendThis=new XMLHttpRequest();
+                                        
                                         var url = "https://chessconnect.xyz/register/process-register.php";
-                                        var user='{"username":document.getElementById("username").innerHTML,"password":document.getElementById("password").innerHTML,"firstName":document.getElementById("firstName").innerHTML,"lastName":document.getElementById("lastName").innerHTML,"email":document.getElementById("email").innerHTML,"phone":document.getElementById("phone").innerHTML,"country":document.getElementById("country").innerHTML,"chessRating":document.getElementById("chessRating").innerHTML,"favoriteOpening":document.getElementById("favoriteOpening").innerHTML,"title":document.getElementById("title").innerHTML}';
-                                      let jsonPayload=JSON.stringify(user);
-                                      
-                                       sendThis.open('POST',url,true);
-                                       sendThis.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-                                   
-                                        sendThis.send(jsonPayload);
-                                       
+                                        var user={
+                                            username:document.getElementById("username").value,
+                                            password:document.getElementById("password").value,
+                                            firstName:document.getElementById("firstName").value,
+                                            lastName:document.getElementById("lastName").value,
+                                            email:document.getElementById("email").value,
+                                            phone:document.getElementById("phone").value,
+                                            country:document.getElementById("country").value,
+                                            chessRating:document.getElementById("chessRating").value,
+                                            favoriteOpening:document.getElementById("favoriteOpening").value,
+                                            title:document.getElementById("title").value
+                                                };
+                                    
+                                    var UserString = JSON.stringify(sendUserInfo);
+                                    const xhr = new XMLHttpRequest();
+                                    xhr.open("POST", url, true);
+                                    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
                                      
                                       
                                        
