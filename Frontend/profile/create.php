44adf9cc35cd3f318881a708e65533f64e562417
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 13:08:46 -0400

    Update create.php

--- Frontend/profile/create.php
+++ Frontend/profile/create.php
@@ -26,7 +26,7 @@
         $stmt = $conn->prepare("INSERT INTO contacts (user_id, name, email, phone, country, chess_rating, favorite_opening, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
         $stmt->bind_param("issssiss", $userId, $name, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
         $stmt->execute();
-        header("Location: ../profile/");
+        //header("Location: ../profile/");
     }
 
 ?>
