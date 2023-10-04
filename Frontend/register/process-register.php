c8bb1d39112f03e32d376e1c8b69fbc642492667
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 16:20:49 -0400

    Update process-register.php

--- Frontend/register/process-register.php
+++ Frontend/register/process-register.php
@@ -32,11 +32,12 @@ if ($conn->connect_error) {
     $stmt = $conn->prepare("INSERT INTO users (username, password, first_name, last_name, email, phone, country, chess_rating, favorite_opening, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
   
     $stmt->bind_param("ssssssssss", $username, $hashed_password, $firstName, $lastName, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
- if($stmt->execute()){
+ /*if($stmt->execute()){
        header("https://chessconnect.xyz/login/login.php");
         exit();
  }
  $stmt->close();
-   $conn->close();}
+   $conn->close();*/
+}
     
 ?>
