21f80f5b331af93c4e41823e53ce2c8ac650e07b
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 20:13:55 -0400

    Update register.php

--- Frontend/register/register.php
+++ Frontend/register/register.php
@@ -23,12 +23,19 @@
         <!--end of nav-->
 
     <div class="container">
-         
-                 
-           <?php
+                 <?php
+            $username = "";
+            $password = "";
+            $firstName ="";
+            $lastName = "";
+            $email = "";
+            $phone = "";
+            $country = "";
+            $chessRating = 0;
+            $favoriteOpening = "";
+            $title = "";
 
             // Check if the form is submitted
-
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                 // Get the username, password, and additional user information from the form
@@ -87,59 +94,56 @@
                 $stmt->close();
                 $conn->close();
             }
-               
         ?>
-       
-            
+
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card mt-5">
                     <div class="card-header">Register</div>
                     <div class="card-body">
-                        <form>
+                        <form method="POST" action="register.php">
                             <div class="mb-3">
                                 <label for="username" class="form-label">Username</label>
                                 <input type="text" class="form-control" id="username" name="username" required>
                             </div>
                             <div class="mb-3">
                                 <label for="password" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="password" name="password" value=""required>
+                                <input type="password" class="form-control" id="password" name="password" value="<?php echo $password ?>"required>
                             </div>
                             <div class="mb-3">
                                 <label for="firstName" class="form-label">First Name</label>
-                                <input type="text" class="form-control" id="firstName" name="firstName"value="firstName">
+                                <input type="text" class="form-control" id="firstName" name="firstName"value="<?php echo $firstName ?>">
                             </div>
                             <div class="mb-3">
                                 <label for="lastName" class="form-label">Last Name</label>
-                                <input type="text" class="form-control" id="lastName" name="lastName"value="">
+                                <input type="text" class="form-control" id="lastName" name="lastName"value="<?php echo $lastName ?>">
                             </div>
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="email" name="email"value="">
+                                <input type="email" class="form-control" id="email" name="email"value="<?php echo $email ?>">
                             </div>
                             <div class="mb-3">
                                 <label for="phone" class="form-label">Phone</label>
-                                <input type="text" class="form-control" id="phone" name="phone" value="">
+                                <input type="text" class="form-control" id="phone" name="phone" value="<?php echo $phone ?>">
                             </div>
                             <div class="mb-3">
                                 <label for="country" class="form-label">Country</label>
-                                <input type="text" class="form-control" id="country" name="country" value="">
+                                <input type="text" class="form-control" id="country" name="country" value="<?php echo $country?>">
                             </div>
                             <div class="mb-3">
                                 <label for="chessRating" class="form-label">Chess Rating</label>
-                                <input type="number" class="form-control" id="chessRating" name="chessRating"value="">
+                                <input type="number" class="form-control" id="chessRating" name="chessRating"value="<?php echo $chessRating ?>">
                             </div>
                             <div class="mb-3">
                                 <label for="favoriteOpening" class="form-label">Favorite Opening</label>
-                                <input type="text" class="form-control" id="favoriteOpening" name="favoriteOpening"value="">
+                                <input type="text" class="form-control" id="favoriteOpening" name="favoriteOpening"value="<?php echo $favoriteOpening ?>">
                             </div>
                             <div class="mb-3">
                                 <label for="title" class="form-label">Title</label>
-                                <input type="text" class="form-control" id="title" name="title"value="">
+                                <input type="text" class="form-control" id="title" name="title"value="<?php echo $title ?>">
                             </div>
                             <!-- Add fields for other user information (email, country, etc.) -->
-                            <button type="submit" id="formSubmission" class="btn btn-primary">Register</button>
-
+                            <button type="submit" class="btn btn-primary">Register</button>
                         </form>
                     </div>
                 </div>
