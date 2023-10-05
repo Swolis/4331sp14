f0272b13d4801a05f4b4aa31dce59be138e7f2ed
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-04 20:16:53 -0400

    Update create.php

--- Frontend/profile/create.php
+++ Frontend/profile/create.php
@@ -1,37 +1,31 @@
 <?php
-session_start();
-
-if($_SERVER['REQUEST_METHOD']=="POST"){
-   $conn = new mysqli("localhost", "newuser", "StrongerPassword123!", "chesscont");
-   if ($conn->connect_error) {
-            die("Connection failure");
+    session_start();
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $conn = new mysqli("localhost", "newuser", "StrongerPassword123!", "chesscont");
+    
+        // Check the connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        if (!isset($_SESSION['user_id'])) {
+            die("User ID not found in session. Please log in again.");
+        }
+        
+        
+        // Assuming you pass user_id from some other source like session
+        $userId = $_SESSION['user_id'];
+        $name = $_POST['name'];
+        $email = $_POST['email'] ?? null;  // Uses null coalescing for optional fields
+        $phone = $_POST['phone'] ?? null;
+        $country = $_POST['country'] ?? null;
+        $chessRating = $_POST['chessRating'] ?? null;
+        $favoriteOpening = $_POST['favoriteOpening'] ?? null;
+        $title = $_POST['title'] ?? null;
+    
+        $stmt = $conn->prepare("INSERT INTO contacts (user_id, name, email, phone, country, chess_rating, favorite_opening, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("issssiss", $userId, $name, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
+        $stmt->execute();
+        header("Location: ../profile/");
     }
 
-  if(!isset($_SESSION['user_id'])){
-    header("https://chessconnect.xyz/");
-  }
-   
-   
-$userid=$_SESSION['user_id'];
-  $name=$_GET['name'];
-  $email=$_GET['email']??null;
-  $phone=$_GET['phone']??null;
-  $country=$_GET['country']??null;
-  $chessRating=$_GET['chessRating']??null;
-  $favoriteOpening=$_GET['favoriteOpening']??null;
-  $title=$_GET['title']??null;
-   $stmt = $conn->query("INSERT INTO contacts (user_id,name, email, phone, country, chess_rating, favorite_opening, title) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?)");
- 
-   $stmt->bind_param("isssssss", $userid,$name, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
-   
-   if( $stmt->execute()){
-
- header("https://chessconnect.xyz/");
-      exit();
-   }else{
-       header("https://chessconnect.xyz");
-   }
-   $stmt->close();
-   $conn->close();
-}
 ?>
