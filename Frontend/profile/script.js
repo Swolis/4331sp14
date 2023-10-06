a0f88317959c810f052feecd14c359e0a814d785
mSpiker1 <mspiker@embarqmail.com>
2023-10-05 23:50:41 -0400

    cleaned up js code a little bit

--- Frontend/profile/script.js
+++ Frontend/profile/script.js
@@ -63,65 +63,6 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         }
     });
-    
-    // Click event for all 'edit' buttons
-    document.querySelectorAll('.edit-button').forEach(button => {
-        button.addEventListener('click', function() {
-            var id = this.getAttribute('data-id');
-            console.log("Checking ID:", id);
-            document.querySelector('#data-' + id).style.display = 'none'; // Hide the data row
-            document.querySelector('#edit-' + id).style.display = ''; // Show the edit row
-            
-            fields.forEach(field => {
-                var el = document.querySelector('#edit-' + field + '-' + id);
-                if (el) el.contentEditable = true;
-            });
-        });
-    });
-
-    // Click event for all 'end editing' buttons
-document.querySelectorAll('.end-editing').forEach(button => {
-    button.addEventListener('click', function() {
-        var id = this.getAttribute('data-id');
-
-        saveText(id, function(response) {
-            if (response.status === 'success') {
-                // On success, update the visible data
-                fields.forEach(field => {
-                    var inputEl = document.querySelector('#edit-' + id + ' input[name=' + field + ']');
-                    if (inputEl) {
-                        document.querySelector('#data-' + id + ' .' + field).textContent = inputEl.value.trim();
-                    }
-                });
-
-                // Switch visibility
-                document.querySelector('#data-' + id).style.display = ''; // Show the data row
-                document.querySelector('#edit-' + id).style.display = 'none'; // Hide the edit row
-            } else {
-                // Handle error - e.g., show an error message to the user
-                console.log("You got an error");
-                console.log(response.message);
-            }
-        }); 
-    });
-});
-
-    // Click event for all 'delete' buttons
-document.querySelectorAll('.delete-button').forEach(button => {
-    button.addEventListener('click', function() {
-        var id = this.getAttribute('data-id');
-        deleteRecord(id, function(response) {
-            if (response.status === 'success') {
-                // On success, remove the row from the table
-                document.querySelector('#data-' + id).remove();
-                document.querySelector('#edit-' + id).remove();
-            } else {
-                // Handle error - e.g., show an error message to the user
-                console.error("Error deleting record:", response.message);
-            }
-        });
-    });
-});
 
 function deleteRecord(id, callback){
     var xr = new XMLHttpRequest();
