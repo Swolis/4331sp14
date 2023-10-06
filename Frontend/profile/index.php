28eaee41d3915c06e4c460c44732c637847ed052
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-06 00:49:03 -0400

    Update index.php

--- Frontend/profile/index.php
+++ Frontend/profile/index.php
@@ -307,13 +307,6 @@ $userDetails = [
             });
         </script>
         <script src = "script.js"> </script>
-        <?php 
-$contacts_query = "SELECT * FROM contacts WHERE user_id = ?";
-    $stmt = $conn->prepare($contacts_query);
-    $stmt->bind_param("i", $_SESSION["user_id"]);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $contacts = $result->fetch_all(MYSQLI_ASSOC);
-?>
+
     </body>
 </html>
