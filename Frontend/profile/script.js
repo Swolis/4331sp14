f81874762a8080846859b45d5a88f79e3c7381d7
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-07 19:35:44 -0400

    Update script.js

--- Frontend/profile/script.js
+++ Frontend/profile/script.js
@@ -17,7 +17,7 @@ function addContact() {
 	// http POST : Attempt to send json with new-account login and pasword data to server.	
 	var xhr = new XMLHttpRequest();
 	
-	xhr.open("POST", baseurl+extention,true);
+	xhr.open("POST", baseurl+extention,false);
 	
 	console.log(xhr);
 	xhr.setRequestHeader("Content-type", "application/json");
