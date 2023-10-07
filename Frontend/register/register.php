c68a5b96d7414fa98f77baf406e95ed4b50baca3
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-06 21:25:05 -0400

    Update register.php

--- Frontend/register/register.php
+++ Frontend/register/register.php
@@ -143,7 +143,7 @@
                                 <input type="text" class="form-control" id="title" name="title"value="<?php echo $title ?>">
                             </div>
                             <!-- Add fields for other user information (email, country, etc.) -->
-                            <button type="submit" class="btn btn-primary">Register</button>
+                            <button type="submit" class="btn btn-primary" onclick="addUser()">Register</button>
                         </form>
                     </div>
                 </div>
