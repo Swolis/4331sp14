ba0a0c14f09533c7608f62452b4f297c355a7e95
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-07 17:44:15 -0400

    Update update.php

--- Frontend/profile/update.php
+++ Frontend/profile/update.php
@@ -1,1 +1,9 @@
-
+  <?php
+  session_start();                              
+$contacts_query = "SELECT * FROM contacts WHERE user_id = ?";
+$stmt = $conn->prepare($contacts_query);
+$stmt->bind_param("i", $_SESSION["user_id"]);
+$stmt->execute();
+ $result = $stmt->get_result();
+$contacts = $result->fetch_all(MYSQLI_ASSOC);
+?>
