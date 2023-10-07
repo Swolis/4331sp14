ec92f4c53fe60f981a3eba5f4c6066ac01ee3c20
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-07 00:50:53 -0400

    Update register.php

--- Frontend/register/register.php
+++ Frontend/register/register.php
@@ -76,14 +76,8 @@
                                 <input type="text" class="form-control" id="title" name="title">
                             </div>
                             <!-- Add fields for other user information (email, country, etc.) -->
-                            <button type="submit" class="btn btn-primary" onclick=<script>
-                                if(addUser()){
-                                <php?>
-                                    header("Location: .login/login.php");
-                                    ?>
-                                }
-                                
-                            </script></button></button>Register</button>
+                            <button type="submit" class="btn btn-primary" onclick="addUser();"</button></button>Register</button>
+                            
                         </form>
                     </div>
                 </div>
