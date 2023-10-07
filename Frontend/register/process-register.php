c7391a19740505e874c46f68fd5746d7244239aa
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-07 10:53:55 -0400

    Update process-register.php

--- Frontend/register/process-register.php
+++ Frontend/register/process-register.php
@@ -47,7 +47,7 @@ $select = mysqli_query($conn, "SELECT * FROM users WHERE username = '".$data['us
                 // Execute the statement
                 if (!mysqli_num_rows($select)&&$stmt->execute()&&$username!=""&&$password!="") {
                     // Registration successful, redirect to the login page
-                    
+                    return header("Location:https://chessconnect.xyz/login/login.php")
                     exit();
                 }else {
                     // Registration failed, redirect back to the registration page with an error
@@ -79,6 +79,7 @@ $select = mysqli_query($conn, "SELECT * FROM users WHERE username = '".$data['us
                 $retValue = '{"error":"' . $err . '"}';
                 sendResultInfoAsJson( $retValue );
                 }
+
         ?>
 
 
