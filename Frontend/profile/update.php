f473446a384afb21f41c452df0c4aede74962a50
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-07 17:46:12 -0400

    Update update.php

--- Frontend/profile/update.php
+++ Frontend/profile/update.php
@@ -1,9 +1,16 @@
   <?php
+// Establish the database connection
+session_start();   
+$conn = new mysqli("localhost", "newuser", "StrongerPassword123!", "chesscont");
 
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
  if (!isset($_SESSION['user_id'])) {
             returnWithError("User ID not found in session. Please log in again.");
         }
-  session_start();                              
+                             
 $contacts_query = "SELECT * FROM contacts WHERE user_id = ?";
 $stmt = $conn->prepare($contacts_query);
 $stmt->bind_param("i", $_SESSION["user_id"]);
