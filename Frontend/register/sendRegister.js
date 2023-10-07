b9bd2875193212158b018b434969a217d9abe4bc
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-07 00:11:19 -0400

    Update sendRegister.js

--- Frontend/register/sendRegister.js
+++ Frontend/register/sendRegister.js
@@ -25,7 +25,7 @@ function addUser()
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", baseurl+extention, true);
 	console.log(xhr);
-	xhr.setRequestHeader("Content-type", "text/html");
+	xhr.setRequestHeader("Content-type", "application/json");
 	try
 	{
 		xhr.send(jsonPayload);
