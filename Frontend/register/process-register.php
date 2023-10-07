a5addb111e78a3a4f84f2bac42a8b7fe2524955c
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-07 00:03:16 -0400

    Update process-register.php

--- Frontend/register/process-register.php
+++ Frontend/register/process-register.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
-
+header('Content-Type: application/json');
 error_reporting(E_ALL); // reports all errors
 ini_set("display_errors", "1"); // shows all errors
 ini_set("log_errors", 1);
