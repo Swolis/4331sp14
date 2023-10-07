d1bff89dfe3b405839b3488efae4dc53fac1e135
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-07 13:46:56 -0400

    Update sendRegister.js

--- Frontend/register/sendRegister.js
+++ Frontend/register/sendRegister.js
@@ -23,7 +23,9 @@ function addUser()
 	console.log(username);
 	// http POST : Attempt to send json with new-account login and pasword data to server.	
 	var xhr = new XMLHttpRequest();
+	
 	xhr.open("POST", baseurl+extention,false);
+	xhr.responseType = 'text';
 	console.log(xhr);
 	xhr.setRequestHeader("Content-type", "application/json");
 	try{
