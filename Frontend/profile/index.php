c1398cde9b42d3c60d4bf8abba1faf234c3fd0dd
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-08 13:34:57 -0400

    Update index.php

--- Frontend/profile/index.php
+++ Frontend/profile/index.php
@@ -77,7 +77,7 @@
             });
         </script>
             <script>
-                    document.documentElement.create.addEventListener('click',function(){
+                   document.getElementById("create").addEventListener('click',function(){
                         var result =addContact();
                         if(result){
                         setTimeout(function(){location.reload()}, 3000);
