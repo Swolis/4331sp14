961d912f3574472aea44a406c6a449de9409410d
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-08 18:24:12 -0400

    Update index.php

--- Frontend/profile/index.php
+++ Frontend/profile/index.php
@@ -100,7 +100,7 @@
             <!--end of nav-->
               <div class="navbar">
         <a class="navbar-brand" href="#">Chess Connect</a>
-        <a class="nav-link" href="../register/register.php">Register</a>
+        <a class="nav-link" href="../login/login.php">Log Out</a>
                 </div>
             <!--beginning of the users profile info -->
             <div class="container mt-5">
