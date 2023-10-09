a1221cb58aa2626e9ad18164cd07b968289da6f6
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-10-08 22:02:12 -0400

    Update display.php

--- Frontend/profile/display.php
+++ Frontend/profile/display.php
@@ -6,4 +6,21 @@ $data=getRequestInfo();
     $stmt->execute();
     $result = $stmt->get_result();
     $contacts = $result->fetch_all(MYSQLI_ASSOC);
+
+
+    function getRequestInfo(){
+        return json_decode(file_get_contents('php://input'), true);
+    }
+        
+    // send json
+    function sendResultInfoAsJson($obj){
+        header('Content-type: text/html');
+        echo $obj;
+    }
+        
+    // return json with error message
+    function returnWithError($err){
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
 ?>
